430e10401b1b9568b6c37c0493049e29e939b4de
CarlosPereira <carlos.pereira21ofc@gmail.com>
2026-07-01 20:07:20 -0300

    Adição de notebook para processamento de dados de natação e arquivos de dados relacionados
    - Criação de um Jupyter notebook () para processar tempos de natação.
    - Implementação de um script Python () para ler e analisar dados de natação.
    - Adição de vários arquivos de dados de natação, abrangendo diferentes nadadores e provas, no diretório .

---
+++ ConceitosPython/loop_while.py
@@ -0,0 +1,9 @@
+frutas = ["maçã", "banana", "uva"]
+i = 1
+idx = 0
+
+while i <= len(frutas):
+    fruta = frutas[idx]
+    print(fruta)
+    idx += 1
+    i += 1
---
+++ jupyter_notebook/swimclub.py
@@ -0,0 +1,36 @@
+import statistics
+
+FOLDER = "swimdata/"
+
+def read_swim_data(filename):
+    # 1. Desestrutura o nome do arquivo
+    nome, categoria, distancia, estilo = filename.removesuffix(".txt").split("-")
+
+    # 2. Lê os tempos do arquivo
+    with open(FOLDER + filename, "r") as arquivo: #O "r" nessa linha é para indicar que o arquivo será apenas lido(read) e não modificado
+        linhas = arquivo.readlines()
+        tempos = linhas[0].strip().split(",")
+
+    # 3. Converte os tempos para centésimos de segundo (inteiros)
+    convercoes = []
+    for t in tempos:
+        minutos, resto = t.split(":")
+        segundos, centesimos = resto.split(".")
+        int_conversao = (
+            (int(minutos) * 60 * 100) + (int(segundos) * 100) + (int(centesimos))
+        )
+        convercoes.append(int_conversao)
+
+    # 4. Calcula a média (mantendo em centésimos como inteiro)
+    media_centesimos = round(statistics.mean(convercoes))
+
+    # 5. Reconverte de centésimos para o formato MM:SS.hh de forma segura
+    minutos = media_centesimos // 6000
+    resto_segundos = media_centesimos % 6000
+    segundos = resto_segundos // 100
+    centesimos = resto_segundos % 100
+
+    # O formato :02d garante que números menores que 10 ganhem um zero à esquerda (ex: 05)
+    media_formatada = f"{minutos}:{segundos:02d}.{centesimos:02d}"
+
+    return nome, categoria, distancia, estilo, tempos, media_formatada
