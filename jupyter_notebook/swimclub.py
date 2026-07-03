5d6d59da55564c64e6c4dce95ebd51f0d4b278fc
CarlosPereira <carlos.pereira21ofc@gmail.com>
2026-07-03 16:54:09 -0300

    - Files.ipynb: Foi adicionado um loop para numerar e imprimir todos os arquivos da variavel swim_files. - swimclub.py: Adição de if/else para identificar se os dados dos tempos das pastas dos alunos tem mais de 1 minuto ou não. - TESTE.ipynb: É um arquivo de teste utilizado para fazer os exercícios e testar códigos no notebook jupyter. - Times.ipynb: Não houve alteração eu só mudei a constante FN para fazer testes, mas já coloquei o nome do arquivo que estava de volta.

--- jupyter_notebook/swimclub.py
+++ jupyter_notebook/swimclub.py
@@ -14,21 +14,33 @@ def read_swim_data(filename):
     # 3. Converte os tempos para centésimos de segundo (inteiros)
     convercoes = []
     for t in tempos:
-        minutos, resto = t.split(":")
-        segundos, centesimos = resto.split(".")
-        int_conversao = (
-            (int(minutos) * 60 * 100) + (int(segundos) * 100) + (int(centesimos))
-        )
-        convercoes.append(int_conversao)
+        if ":" in t:    
+            minutos, resto = t.split(":")
+            segundos, centesimos = resto.split(".")
+        
+        else:
+            minutos = 0
+            segundos, centesimos = t.split(".")
+
+    int_conversao = ((int(minutos) * 60 * 100) + (int(segundos) * 100) + (int(centesimos)))
+    convercoes.append(int_conversao)
 
     # 4. Calcula a média (mantendo em centésimos como inteiro)
     media_centesimos = round(statistics.mean(convercoes))
 
     # 5. Reconverte de centésimos para o formato MM:SS.hh de forma segura
-    minutos = media_centesimos // 6000
-    resto_segundos = media_centesimos % 6000
+
+    #Centesimos -> Minutos
+    minutos = media_centesimos // (60 * 100)
+
+    #Centesimos -> segundos com os min`s inclusos
+    resto_segundos = media_centesimos % (60 * 100)
+
+    #Segundos retirando os minutos (ex.: 80 segundos - 1 min = 20 segundos) 
     segundos = resto_segundos // 100
-    centesimos = resto_segundos % 100
+    
+    #O resto dos segundos, ou seja o que vem depois da vírgula são os centésimos 
+    centesimos = resto_segundos % 100   
 
     # O formato :02d garante que números menores que 10 ganhem um zero à esquerda (ex: 05)
     media_formatada = f"{minutos}:{segundos:02d}.{centesimos:02d}"
